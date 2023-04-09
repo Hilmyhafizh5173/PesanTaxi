59754836258ba12e9782a99b8f1435e38409a4b8
Hilmyhafizh5173 <128022469+Hilmyhafizh5173@users.noreply.github.com>
2023-04-09 20:03:47 +0700

    Add files via upload

---
+++ Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesanTaxi
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo() 
+        {
+            string duty = "";
+            if (OnDuty == true) 
+            {
+                duty = "yes";
+            }else
+            {
+                duty="no";
+            }
+
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", duty);
+            Console.WriteLine("Number of passenger: {0}", NumPassenger);
+        }
+        public void PickUpPassenger() 
+        {
+            Console.WriteLine("\nJono sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger() 
+        {
+            Console.WriteLine("Jono selesai menjemput penumpang\n", DriverName);
+        }
+
+    }
+}
